7f8a82edb52938d465b67301cc616d0fc0eca847
JuanPa329~ <Juanpablomurcia150@gmail.com>
2026-05-13 15:23:54 -0500

    Fix eslint warning

--- client/src/App.js
+++ client/src/App.js
@@ -31,6 +31,7 @@ function App() {
 
   useEffect(() => {
     fetchEmpleados();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const showMessage = (text, type = 'success') => {
